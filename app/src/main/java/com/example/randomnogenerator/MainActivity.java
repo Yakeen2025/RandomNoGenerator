b251719ab268ee0898c024b22e59bd0c8d8f5fbd
khan <yakeen625@gmail.com>
2020-03-30 11:25:37 +0530

    second

--- app/src/main/java/com/example/randomnogenerator/MainActivity.java
+++ app/src/main/java/com/example/randomnogenerator/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.randomnogenerator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +20,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     EditText editText;
     Button button;
+    View view;
+    int count=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         editText=findViewById(R.id.txt);
         button=findViewById(R.id.btn1);
+        view=findViewById(R.id.li);
         final Random random=new Random();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +40,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
 
-
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        count+=1;
+        if (count%2!=0){
+        if (item.getItemId()==R.id.back){
+            view.setBackgroundColor(getResources().getColor(R.color.red));
+        }}
+        else{
+            view.setBackgroundColor(Color.WHITE);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
